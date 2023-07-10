29fcecc99b860c89e5ec8fd02181f0e1c577b513
Buger-od-ua <buheriaoleksii@gmail.com>
2023-07-10 21:38:07 +0300

    Auth

--- src/main/java/com/example/sweater/Application.java
+++ src/main/java/com/example/sweater/Application.java
@@ -4,7 +4,14 @@ package com.example.sweater;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
+@ComponentScan("com.example.sweater")
 @SpringBootApplication
 public class Application {
 
